d9879083895acd235dd65ac164e569995c2cec03
MarkivNazar <nazarmn2008@gmail.com>
2025-02-06 22:34:21 +0200

    fixe style CEOAIModels

--- src/components/CEOAIComponents/CEOAIModels/CEOAIModels.jsx
+++ src/components/CEOAIComponents/CEOAIModels/CEOAIModels.jsx
@@ -1,41 +1,31 @@
 import React, { useState } from 'react';
 import './CEOAIModels.css';
 import CEO1 from '/CEO1.png';
-import vite from '/vite.svg';
 
 const CEO_IMAGES = [
   { id: 'CEO1', src: CEO1 },
-  { id: 'CEO2', src: vite }
 ];
 
 export default function CEOAIModels() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedCEO, setSelectedCEO] = useState(null);
 
-  const nextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % CEO_IMAGES.length);
-  };
-
-  const prevImage = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + CEO_IMAGES.length) % CEO_IMAGES.length
-    );
+  const handleSelect = (id) => {
+    setSelectedCEO(prevSelected => (prevSelected === id ? null : id));
   };
 
   return (
     <div className="CEO-AI-Models">
       <header className="CEO-AI-Models-header">Choose your CEO design</header>
       <div className="CEO-AI-Models-Container">
-        <div className="CEO-Slide">
-          <img
-            src={CEO_IMAGES[currentIndex].src}
-            alt={`CEO Model ${CEO_IMAGES[currentIndex].id}`}
-            className="CEO-Image"
-          />
-        </div>
-      </div>
-      <div className="CEO-AI-Models-Controls">
-        <button onClick={prevImage} className="Slider-Button">Previous</button>
-        <button onClick={nextImage} className="Slider-Button">Next</button>
+        {CEO_IMAGES.map((image) => (
+          <div key={image.id} className="CEO-Slide" onClick={() => handleSelect(image.id)}>
+            <img
+              src={image.src}
+              alt={`CEO Model ${image.id}`}
+              className={`CEO-Image ${selectedCEO === image.id ? 'selected' : ''}`}
+            />
+          </div>
+        ))}
       </div>
       <div className="CEO-AI-Models-Save">
         <button>Continue</button>
