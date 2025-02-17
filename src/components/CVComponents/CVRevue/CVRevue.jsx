a864ccc6f5bc53a894e77b37846b7b16b8b34a46
MarkivNazar <nazarmn2008@gmail.com>
2025-02-17 23:05:56 +0200

    fixe style CVRevue

--- src/components/CVComponents/CVRevue/CVRevue.jsx
+++ src/components/CVComponents/CVRevue/CVRevue.jsx
@@ -1,27 +1,23 @@
-
 import React from 'react';
 import './CVRevue.css';
-
 
 export default function CVRevue() {
   return (
     <div className="CV-Revue"> 
       <header className="CV-Revue-header">
-        <div className="CV-Editing-header-Left">
-          <img src="" alt="" />
+        <div className="CV-Revue-header-Left">
+          <img src="https://avatars.githubusercontent.com/u/69341802?v=4" alt="Profile" className="profile-img" />
         </div>
 
         <div className="CV-Revue-header-Right">
           <h2>Name</h2>
-          <p>CV</p>
-          
+          <p>IT</p>
         </div>
         <div className="triangle"></div>
       </header>
 
       <div className="CV-Revue-Container">
         <div className="CV-Revue-Container-Left"></div>
-
         <div className="CV-Revue-Container-Right"></div>
       </div>     
     </div>
