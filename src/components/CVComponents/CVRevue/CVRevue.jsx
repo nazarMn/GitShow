f90c04f95a28888157b78cc03f5b51e1dcfeeeb0
MarkivNazar <nazarmn2008@gmail.com>
2025-02-19 19:30:15 +0200

    added styles for Contact

--- src/components/CVComponents/CVRevue/CVRevue.jsx
+++ src/components/CVComponents/CVRevue/CVRevue.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './CVRevue.css';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone,faLocationDot } from "@fortawesome/free-solid-svg-icons";
+<FontAwesomeIcon icon="fa-solid fa-envelope" />
 export default function CVRevue() {
   return (
     <div className="CV-Revue"> 
@@ -19,9 +21,9 @@ export default function CVRevue() {
       <div className="CV-Revue-Container">
         <div className="CV-Revue-Container-Left">
           <div className="Contact-Сontainer">
-            <div className="Contact-Email">Email</div>
-            <div className="Contact-Phone">Phone</div>
-            <div className="Contact-Adress">Adress</div>
+            <div className="Contact-Email"> <FontAwesomeIcon icon={faEnvelope}  size="xl" /> <h2>Email</h2></div>
+            <div className="Contact-Phone"> <FontAwesomeIcon icon={faPhone}  size="xl" /> <h2>Phone</h2></div>
+            <div className="Contact-Adress"><FontAwesomeIcon icon={faLocationDot}  size="xl" /> <h2>Adress</h2></div>
 
           </div>
           <div className="Education-Container">
