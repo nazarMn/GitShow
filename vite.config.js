f3fe86d8a35bd68a5487f1cef4e4164b3b1dbfb5
MarkivNazar <nazarmn2008@gmail.com>
2025-02-21 09:43:32 +0200

    added hosting option

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.REACT_APP_API_URL": JSON.stringify("https://your-api.onrender.com")
+  },
 })
