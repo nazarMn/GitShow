432c810e028eb4400aba46f2a3fba635a8d0d7ea
MarkivNazar <nazarmn2008@gmail.com>
2025-02-21 15:54:06 +0200

    fixe

--- src/components/HomePage/Setting/ResumeSettings/ResumeSettings.jsx
+++ src/components/HomePage/Setting/ResumeSettings/ResumeSettings.jsx
@@ -12,7 +12,6 @@ export default function ResumeSettings() {
   const [editing, setEditing] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [items, setItems] = useState([]);
-  const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000';
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
@@ -30,7 +29,7 @@ export default function ResumeSettings() {
     }
 
     try {
-      const response = await fetch(`${API_BASE_URL}/api/resumes`, {
+      const response = await fetch('http://localhost:3000/api/resumes', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
@@ -65,7 +64,7 @@ export default function ResumeSettings() {
     }
   
     try {
-      const response = await fetch(`${API_BASE_URL}/api/resumes/${formData.id}`, {
+      const response = await fetch(`http://localhost:3000/api/resumes/${formData.id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -90,7 +89,7 @@ export default function ResumeSettings() {
   
   const handleDelete = async (id) => {
     try {
-      const response = await fetch(`${API_BASE_URL}/api/resumes/${id}`, {
+      const response = await fetch(`http://localhost:3000/api/resumes/${id}`, {
         method: 'DELETE',
       });
   
@@ -109,7 +108,7 @@ export default function ResumeSettings() {
 
   const handleExperienceUpdate = async () => {
     try {
-      const response = await fetch(`${API_BASE_URL}/api/user`, {
+      const response = await fetch('http://localhost:3000/api/user', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
@@ -142,7 +141,7 @@ export default function ResumeSettings() {
   useEffect(() => {
     const fetchResumes = async () => {
       try {
-        const response = await fetch(`${API_BASE_URL}/api/resumes`, {
+        const response = await fetch('http://localhost:3000/api/resumes', {
           credentials: 'include', // Це дозволяє передавати сесію з браузера
         });
         if (response.ok) {
@@ -169,7 +168,7 @@ export default function ResumeSettings() {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await fetch(`${API_BASE_URL}/api/user`, {
+        const response = await fetch('http://localhost:3000/api/user', {
           credentials: 'include',
         });
         if (response.ok) {
