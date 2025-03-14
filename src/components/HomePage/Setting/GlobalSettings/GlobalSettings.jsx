7a52487b45c80f18d0b409fe3177a064af637cb5
MarkivNazar <nazarmn2008@gmail.com>
2025-03-14 20:25:26 +0200

    added styles for GlobalSettings

--- src/components/HomePage/Setting/GlobalSettings/GlobalSettings.jsx
+++ src/components/HomePage/Setting/GlobalSettings/GlobalSettings.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './GlobalSettings.css'
+import SettingsSidebar from '../SettingsSidebar/SettingsSidebar'
 
 export default function GlobalSettings() {
   return (
     <div className='GlobalSettings'>
+
+      <SettingsSidebar />
+
+      <div className="GlobalSettings-Content">
+
+      <div className="GlobalSettings-Main">
+        <h1>Global settings</h1>
+        </div>
+
+      </div>
       
     </div>
   )
