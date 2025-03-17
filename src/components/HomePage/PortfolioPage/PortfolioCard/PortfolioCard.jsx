5a755c05e283a00c80fa455e7a14fbaba7e22616
MarkivNazar <nazarmn2008@gmail.com>
2025-03-17 15:34:16 +0200

    added a stab to the photo in Portfolio Card

--- src/components/HomePage/PortfolioPage/PortfolioCard/PortfolioCard.jsx
+++ src/components/HomePage/PortfolioPage/PortfolioCard/PortfolioCard.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './PortfolioCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faPaperclip, faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 export default function PortfolioCard({ title, description, imageUrl, link, websiteUrl, onDelete, onEdit }) {
+  const renderImage = () => {
+    if (imageUrl && imageUrl.trim()) {
+      return <img src={imageUrl} alt="Project Thumbnail" />;
+    }
+
+    const firstLetter = title ? title[0].toUpperCase() : 'P';
+    return (
+      <div className="defaultImage">
+        <span>{firstLetter}</span>
+      </div>
+    );
+  };
+
   return (
     <div className="portfolioCard">
-      <div className="cardImage">
-        <img src={imageUrl || './img/project.jpg'} alt="Project Thumbnail" />
-      </div>
+      <div className="cardImage">{renderImage()}</div>
       <div className="cardOverlay">
         <h2>{title}</h2>
         <p>{description}</p>
@@ -23,18 +34,8 @@ export default function PortfolioCard({ title, description, imageUrl, link, webs
             <FontAwesomeIcon icon={faPaperclip} size="2x" color="#fff" />
           </a>
         )}
-        <FontAwesomeIcon
-          icon={faTrash}
-          size="2x"
-          className="deleteIcon"
-          onClick={onDelete}
-        />
-        <FontAwesomeIcon
-          icon={faEdit}
-          size="2x"
-          className="editIcon"
-          onClick={onEdit} 
-        />
+        <FontAwesomeIcon icon={faTrash} size="2x" className="deleteIcon" onClick={onDelete} />
+        <FontAwesomeIcon icon={faEdit} size="2x" className="editIcon" onClick={onEdit} />
       </div>
     </div>
   );
