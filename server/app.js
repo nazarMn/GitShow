27d95ff1a9fac5aa9bcbb51870a0bbb806608bb1
MarkivNazar <nazarmn2008@gmail.com>
2025-03-19 17:30:31 +0200

    the server url has been changed

--- server/app.js
+++ server/app.js
@@ -817,5 +817,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
