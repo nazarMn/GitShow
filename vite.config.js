62839662ed2bc5fd3402f760c0caf519acdaa134
MarkivNazar <nazarmn2008@gmail.com>
2025-03-23 22:47:45 +0200

    fixe

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Додаємо base для правильних шляхів
+  build: {
+    outDir: 'dist', // Вказуємо вихідну папку для білду
+  },
+  server: {
+    https: true, // Це включить HTTPS для локальної розробки
+  }
 })
