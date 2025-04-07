22ad60e5b16fe06674147932a89b1757e0100d98
MarkivNazar <nazarmn2008@gmail.com>
2025-04-07 18:44:44 +0300

    Added a panel that displays the number of subscriptions and followers for Public Home

--- src/components/ProfilePublicContainer/ProfilePublicHome/PublicHome.jsx
+++ src/components/ProfilePublicContainer/ProfilePublicHome/PublicHome.jsx
@@ -105,6 +105,28 @@ export default function PublicHome() {
               </AreaChart>
             </ResponsiveContainer>
           </div>
+
+          <div className="followsContainer">
+
+<div className="followsCard">
+  <h4>Following</h4>
+  <div className="followsInfo">
+    <h3>0</h3>
+    <p>View all</p>
+  </div>
+</div>
+
+<div className="followsCard">
+  <h4>Followers</h4>
+  <div className="followsInfo">
+    <h3>0</h3>
+    <p>View all</p>
+  </div>
+</div>
+
+
+</div>
+          
         </div>
       </div>
     </div>
